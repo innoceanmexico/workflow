07476d69079c7b2e0f16bb8941c9335c704ec101
Axel <axosar@gmail.com>
2021-01-28 13:46:14 -0600

    Adding icons

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ const Layout = ({children}) => (
     <Head>
       <meta charSet="utf-8" />
       <title>INNOCEAN MÉXICO</title>
-      <link rel="icon" type="image/png" href="/favicon.png" />
+      <link rel="icon" type="image/png" href="favicon.png" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;900&display=swap" rel="stylesheet" /> 
     </Head>
