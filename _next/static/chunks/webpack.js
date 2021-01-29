c97ea4bb2f89eedfb830a61171b7fcf2b9e8a5f3
Axel <axosar@gmail.com>
2021-01-29 12:23:45 -0600

    Deploy Next.js to gh-pages

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "080bdf449db43052c47c";
+/******/ 	var hotCurrentHash = "4702855b279bd450fa11";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
