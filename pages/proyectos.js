33fa4f30c25415dcbcb2c68609b7151eb33ef25f
Axel <axosar@gmail.com>
2021-01-29 13:29:41 -0600

    Proyectos

--- pages/proyectos.js
+++ pages/proyectos.js
@@ -19,13 +19,44 @@ export default function Proyectos() {
         <hr/>
         <div className="marcas">
           <div>
+            <hr/>
             <h2>KIA</h2>
+            <hr/>
             <h3>URL: https://www.kia.com/</h3>
             <h3>URL: https://www.kia.com.mx/</h3>
           </div>
           <div>
+            <hr/>
             <h2>HYUNDAI</h2>
+            <hr/>
             <h3>URL: https://www.hyundai.com.mx/</h3>
+          </div>
+        </div>
+        <div className="marcas">
+          <div>
+            <hr/>
+            <h2>COMEX</h2>
+            <hr/>
+            <h3>URL: https://www.kia.com/</h3>
+            <h3>URL: https://www.kia.com.mx/</h3>
+          </div>
+          <div>
+            <hr/>
+            <h2>CINSA</h2>
+            <hr/>
+            <h3>URL: https://www.hyundai.com.mx/</h3>
+          </div>
+        </div>
+        <div className="marcas">
+          <div>
+            <hr/>
+            <h2>SANTA ANITA</h2>
+            <hr/>
+            <h3>URL: https://www.kia.com/</h3>
+            <h3>URL: https://www.kia.com.mx/</h3>
+          </div>
+          <div>
+            <hr/>
           </div>
         </div>
       </div>
